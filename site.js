4f93d79c4e7eaa675c109dba41d0b16291f5c236
COReichl <reichlcolin@gmail.com>
2026-03-15 11:20:39 -0500

    started getNextQuestion function

--- site.js
+++ site.js
@@ -11,11 +11,22 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 ;(async () => {
 
 	// todo: create your "getNextQuestion" function
-
-	// todo: create your "renderQuestion" function
-
+	const getNextQuestion = async => { 
+	const url = 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple'
+	const json = JSON.parse(fetch(url))
+		.then(response => response.json())
+	const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+	const answers = shuffle([ ...incorrect, correct ])
+	return { question, answers, correct }
+	}
+	console.log(await getNextQuestion())
+	// // todo: create your "renderQuestion" function
+	// const renderQuestion = ({ question, answers, correct }) => {
+	// 	questionElement.textContent = decodeHtml(question)
+	// 	answersElement.textContent = decodeHtml(answers)
+	// 	console.log(correct)
+	// }
 	// todo: add the event listener to the "nextQuestion" button
-
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
